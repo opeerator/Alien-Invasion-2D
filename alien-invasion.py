fd005756e00671b6c9439180cfe7a6b61f3d9924
Ali Yamini <d.aliyamini@gmail.com>
2019-07-12 10:05:18 +0430

    Adding bullets - issue

--- alien-invasion.py
+++ alien-invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -7,18 +8,20 @@ def run_game():
     # Initialize game and create a screen object.
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode(
-            (ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
 run_game()
