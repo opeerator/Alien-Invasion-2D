40fb3eb891534e15f316791af76b183e218ee72a
Ali Yamini <d.aliyamini@gmail.com>
2019-07-16 10:36:08 +0430

    Game over mechanism

--- game_functions.py
+++ game_functions.py
@@ -15,19 +15,22 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
 
 def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     """Respond to ship being hit by alien."""
-    # Decrement ships_left.
-    stats.ships_left -= 1
+    if stats.ships_left > 0:
+        # Decrement ships_left.
+        stats.ships_left -= 1
 
-    # Empty the list of aliens and bullets.
-    bullets.empty()
-    aliens.empty()
+        # Empty the list of aliens and bullets.
+        bullets.empty()
+        aliens.empty()
 
-    # Create a new fleet and center the ship.
-    create_fleet(ai_settings, screen, ship, aliens)
-    ship.center_ship()
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
-    # Pause.
-    sleep(0.5)
+        # Pause.
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
 def check_fleet_edges(ai_settings, aliens):
     """Respond appropriately if any aliens have reached on edge."""
