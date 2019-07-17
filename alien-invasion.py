446fc6f8408d68fb52da88088e1439f674a56fda
Ali Yamini <d.aliyamini@gmail.com>
2019-07-17 14:29:35 +0430

    Linking button to game

--- alien-invasion.py
+++ alien-invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from game_stats import Gamestats
+from button import Button
 
 def run_game():
     # Initialize game and create a screen object.
@@ -12,6 +13,9 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make the play button.
+    play_button = Button(ai_settings, screen, "Play")
+    
     # Create an instance to store game statistics.
     stats = Gamestats(ai_settings)
 
@@ -33,6 +37,6 @@ def run_game():
                 ship.update()
                 gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
                 gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
         
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -150,11 +150,12 @@ def check_events(ai_settings, screen, ship, bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ai_settings, screen, ship, bullets)
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update the screen during each pass through the loop."""
     #Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
 
+    
     #Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
@@ -162,6 +163,10 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     ship.blitme()
     aliens.draw(screen)
     
+    # Draw the play button if the game is inactive.
+    if not stats.game_active:
+        play_button.draw_button()
+        
     #Make the most recently drawn screen visible.
     pygame.display.flip()
 
