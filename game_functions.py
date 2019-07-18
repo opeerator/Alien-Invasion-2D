144568f0fe1ca51bf1490b0d0ae3e285c7644390
Ali Yamini <d.aliyamini@gmail.com>
2019-07-18 09:23:16 +0430

    Deactivating the play button

--- game_functions.py
+++ game_functions.py
@@ -6,7 +6,8 @@ from alien import Alien
 
 def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks play."""
-    if play_button.rect.collidepoint(mouse_x, mouse_y):
+    button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+    if button_clicked and not stats.game_active:
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
